b52773d0530fba32f205661d3d0b3c8837f29040
YonatanAdam <yonatansh07@gmail.com>
2025-01-20 18:54:51 +0200

    Added models, viewmodels and screens

---
+++ Model/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace Model
+{
+    public class Category : BaseEntity
+    {
+        private string name;
+        
+        public string Name { get => name; set => name = value; }
+    }
+}
---
+++ ViewModel/ChangeEntity.cs
@@ -0,0 +1,21 @@
+﻿using Model;
+
+namespace ViewModel
+{
+    
+    public delegate string CreateSQL(BaseEntity entity);
+    public class ChangedEntity
+    {
+        private BaseEntity entity;
+        private CreateSQL createSql;
+
+        public ChangedEntity(CreateSQL createSql, BaseEntity entity)
+        {
+            this.createSql = createSql;
+            this.entity = entity;
+        }
+        
+        public BaseEntity Entity { get => entity; set => entity = value; }
+        public CreateSQL CreateSql { get => createSql; set => createSql = value; }
+    }
+}
